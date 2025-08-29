b07f96c43b8133a224e38f0a7a393f111b1c7ca5
MernStackExpert <mdnirob30k@gmail.com>
2025-08-29 11:33:29 +0600

    first commit

---
+++ style.js
@@ -0,0 +1,48 @@
+
+// const cartButton = document.getElementsByClassName("cart-btn")
+
+// for(const button of cartButton){
+//   button.addEventListener('click' , function(){
+//   alert("hello")
+// })
+// }
+
+
+document.getElementById("product-box-container").addEventListener('click' , function(event){
+   if(event.target.className.includes("cart-btn")){
+    const cartBtn = event.target
+    
+    const productPrice = cartBtn.parentNode.parentNode.children[2].children[0].innerText
+
+    const productImg = cartBtn.parentNode.parentNode.parentNode.children[0].children[0].src
+
+    const productName = cartBtn.parentNode.parentNode.children[0].innerText
+
+    const productCartContainer = document.getElementById("product-cart-container");
+
+    const newCart = document.createElement("div");
+
+    newCart.innerHTML =`
+     <div class="h-[80px] w-full flex justify-between items-center bg-[#f4f1f1] pr-[10px]  mt-[15px] rounded-lg">
+                  <img class="w-[96px] h-[70px]" src="${productImg}" alt="product">
+                  <div class="space-y-5">
+                    <h1 class="font-[600]">${productName}</h1>
+                    <p><span>${productPrice}</span> tk</p>
+                  </div>
+                </div>
+    `
+
+    productCartContainer.append(newCart)
+
+    const totalQuantity = document.getElementById("total-quantity");
+
+    totalQuantity.innerText++
+
+    const totalPrice = document.getElementById("total-price").innerText;
+
+    const finalPrice =  Number(totalPrice) + Number(productPrice);
+
+    document.getElementById("total-price").innerText = finalPrice
+
+   }
+})
